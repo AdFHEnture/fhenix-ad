7a0629d5edbfab81dd26334472d17ed24afdd5d8
d0x471b <0x471@protonmail.com>
2024-07-14 03:19:15 +0300

    fix gas estimation err

--- contracts/adMatcher.sol
+++ contracts/adMatcher.sol
@@ -6,174 +6,185 @@ import "@fhenixprotocol/contracts/FHE.sol";
 import {Permissioned, Permission} from "@fhenixprotocol/contracts/access/Permissioned.sol";
 
 contract AdMatcher is Permissioned {
-    bool[][] public adMatrix;
-    mapping(address => ebool[]) public userVectors;
-    address public owner;
+  event Received(uint32, bytes32, uint, string);
+  bool[][] public adMatrix;
+  mapping(address => ebool[]) public userVectors;
+  address public owner;
 
-    constructor() {
-        owner = msg.sender;
+  constructor() {
+    owner = msg.sender;
 
-        adMatrix.push([true, false, false, false, false]);
-        adMatrix.push([false, false, false, false, false]);
-        adMatrix.push([false, false, false, false, false]);
-        adMatrix.push([true, true, true, true, true]);
-        adMatrix.push([false, false, false, false, false]);
-        adMatrix.push([false, false, true, false, true]);
-        adMatrix.push([true, false, true, false, true]);
-        adMatrix.push([true, false, false, false, true]);
+    adMatrix.push([true, false, false, false, false]);
+    adMatrix.push([false, false, false, false, false]);
+    adMatrix.push([false, false, false, false, false]);
+    adMatrix.push([true, true, true, true, true]);
+    adMatrix.push([false, false, false, false, false]);
+    adMatrix.push([false, false, true, false, true]);
+    adMatrix.push([true, false, true, false, true]);
+    adMatrix.push([true, false, false, false, true]);
+  }
+
+  function addUserVector(bool[] calldata userVector) public {
+    ebool[] memory encryptedUserVector = new ebool[](userVector.length);
+    for (uint256 i = 0; i < userVector.length; i++) {
+      encryptedUserVector[i] = FHE.asEbool(userVector[i]);
+    }
+    userVectors[msg.sender] = encryptedUserVector;
+  }
+
+  function addAd(bool[] calldata ad) public {
+    adMatrix.push(ad);
+  }
+
+  function handle(
+    uint32 _origin,
+    bytes32 _sender,
+    bytes calldata _data
+  ) external payable {
+    bool[] memory adBytesArray = new bool[](_data.length);
+    for (uint i = 0; i < _data.length; i++) {
+      if (_data[i] == 0x00) {
+        adBytesArray[i] = false;
+      } else {
+        adBytesArray[i] = true;
+      }
     }
 
-    function addUserVector(bool[] calldata userVector) public {
-        ebool[] memory encryptedUserVector = new ebool[](userVector.length);
-        for (uint256 i = 0; i < userVector.length; i++) {
-            encryptedUserVector[i] = FHE.asEbool(userVector[i]);
-        }
-        userVectors[msg.sender] = encryptedUserVector;
+    adMatrix.push(adBytesArray);
+    emit Received(_origin, _sender, msg.value, string(_data));
+  }
+
+  function findBestAdFromSenderAddress(
+    Permission memory permission
+  ) public view onlySender(permission) returns (uint256) {
+    euint8[] memory encryptedScores = findBestAdMemory(userVectors[msg.sender]);
+
+    uint8[] memory scores = new uint8[](encryptedScores.length);
+    for (uint256 i = 0; i < encryptedScores.length; i++) {
+      scores[i] = FHE.decrypt(encryptedScores[i]);
     }
 
-    function addAd(bool[] calldata ad) public {
-        adMatrix.push(ad);
+    uint256 bestAdIndex = 0;
+    uint256 bestScore = 0;
+    for (uint256 i = 0; i < scores.length; i++) {
+      if (scores[i] > bestScore) {
+        bestScore = scores[i];
+        bestAdIndex = i;
+      }
     }
 
-    function handle (
-        uint32 _origin,
-        bytes32 _sender,
-        bytes calldata _data
-    ) external payable virtual {
-        bool[] memory adBytesArray = new bool[](_data.length);
-        for (uint i = 0; i < _data.length; i++) {
-            if (_data[i] == 0x00) {
-                adBytesArray[i] = false;
-            } else {
-                adBytesArray[i] = true;
-            }
-        }
+    return bestAdIndex;
+  }
 
-        adMatrix.push(adBytesArray);
+  function findBestAdMemory(
+    ebool[] memory encryptedUserVector
+  ) private view returns (euint8[] memory) {
+    euint8[] memory encryptedScores = new euint8[](adMatrix.length);
+
+    for (uint256 i = 0; i < adMatrix.length; i++) {
+      euint8 sum = FHE.asEuint8(0);
+      for (uint256 j = 0; j < encryptedUserVector.length; j++) {
+        if (adMatrix[i][j]) {
+          sum = sum + FHE.asEuint8(encryptedUserVector[j]);
+        }
+      }
+      encryptedScores[i] = sum;
     }
 
-    function findBestAdFromSenderAddress(Permission memory permission) public view onlySender(permission) returns (uint256) {
-        euint8 [] memory encryptedScores = findBestAdMemory(userVectors[msg.sender]);
+    return encryptedScores;
+  }
 
-        uint8[] memory scores = new uint8[](encryptedScores.length);
-        for (uint256 i = 0; i < encryptedScores.length; i++) {
-            scores[i] = FHE.decrypt(encryptedScores[i]);
+  function findBestAd(
+    inEbool[] calldata encryptedUserVector
+  ) private view returns (euint8[] memory) {
+    euint8[] memory encryptedScores = new euint8[](adMatrix.length);
+
+    for (uint256 i = 0; i < adMatrix.length; i++) {
+      euint8 sum = FHE.asEuint8(0);
+      for (uint256 j = 0; j < encryptedUserVector.length; j++) {
+        if (adMatrix[i][j]) {
+          sum = sum + FHE.asEuint8(FHE.asEbool(encryptedUserVector[j]));
         }
-
-        uint256 bestAdIndex = 0;
-        uint256 bestScore = 0;
-        for (uint256 i = 0; i < scores.length; i++) {
-            if (scores[i] > bestScore) {
-                bestScore = scores[i];
-                bestAdIndex = i;
-            }
-        }
-
-        return bestAdIndex;
+      }
+      encryptedScores[i] = sum;
     }
 
-    function findBestAdMemory(ebool[] memory encryptedUserVector) private view returns (euint8[] memory) {
-        euint8[] memory encryptedScores = new euint8[](adMatrix.length);
+    return encryptedScores;
+  }
 
-        for (uint256 i = 0; i < adMatrix.length; i++) {
-            euint8 sum = FHE.asEuint8(0);
-            for (uint256 j = 0; j < encryptedUserVector.length; j++) {
-                if(adMatrix[i][j]) {
-                    sum = sum + FHE.asEuint8(encryptedUserVector[j]);
-                }
-            }
-            encryptedScores[i] = sum;
-        }
+  function findBestAdPermitSealedFromSenderAddress(
+    Permission memory permission
+  ) public view onlySender(permission) returns (uint256) {
+    euint8[] memory encryptedScores = findBestAdMemory(userVectors[msg.sender]);
 
-        return encryptedScores;
-
+    uint8[] memory scores = new uint8[](encryptedScores.length);
+    for (uint256 i = 0; i < encryptedScores.length; i++) {
+      scores[i] = FHE.decrypt(encryptedScores[i]);
     }
 
-    function findBestAd(inEbool[] calldata encryptedUserVector) private view returns (euint8[] memory) {
-        euint8[] memory encryptedScores = new euint8[](adMatrix.length);
-
-        for (uint256 i = 0; i < adMatrix.length; i++) {
-            euint8 sum = FHE.asEuint8(0);
-            for (uint256 j = 0; j < encryptedUserVector.length; j++) {
-                if(adMatrix[i][j]) {
-                    sum = sum + FHE.asEuint8(FHE.asEbool(encryptedUserVector[j]));
-                }
-            }
-            encryptedScores[i] = sum;
-        }
-
-        return encryptedScores;
-
+    uint256 bestAdIndex = 0;
+    uint256 bestScore = 0;
+    for (uint256 i = 0; i < scores.length; i++) {
+      if (scores[i] > bestScore) {
+        bestScore = scores[i];
+        bestAdIndex = i;
+      }
     }
 
-    function findBestAdPermitSealedFromSenderAddress(Permission memory permission) 
-        public view onlySender(permission) returns (uint256) {
-        euint8 [] memory encryptedScores = findBestAdMemory(userVectors[msg.sender]);
+    return bestAdIndex;
+  }
 
-        uint8[] memory scores = new uint8[](encryptedScores.length);
-        for (uint256 i = 0; i < encryptedScores.length; i++) {
-            scores[i] = FHE.decrypt(encryptedScores[i]);
-        }
+  function findBestAdPermitSealedFromSenderAddressWithoutPermit(
+    address sender
+  ) public view returns (uint256) {
+    euint8[] memory encryptedScores = findBestAdMemory(userVectors[sender]);
 
-        uint256 bestAdIndex = 0;
-        uint256 bestScore = 0;
-        for (uint256 i = 0; i < scores.length; i++) {
-            if (scores[i] > bestScore) {
-                bestScore = scores[i];
-                bestAdIndex = i;
-            }
-        }
-
-        return bestAdIndex;
+    uint8[] memory scores = new uint8[](encryptedScores.length);
+    for (uint256 i = 0; i < encryptedScores.length; i++) {
+      scores[i] = FHE.decrypt(encryptedScores[i]);
     }
 
-    function findBestAdPermitSealedFromSenderAddressWithoutPermit(address sender) public view returns (uint256) {
-        euint8 [] memory encryptedScores = findBestAdMemory(userVectors[sender]);
-
-        uint8[] memory scores = new uint8[](encryptedScores.length);
-        for (uint256 i = 0; i < encryptedScores.length; i++) {
-            scores[i] = FHE.decrypt(encryptedScores[i]);
-        }
-
-        uint256 bestAdIndex = 0;
-        uint256 bestScore = 0;
-        for (uint256 i = 0; i < scores.length; i++) {
-            if (scores[i] > bestScore) {
-                bestScore = scores[i];
-                bestAdIndex = i;
-            }
-        }
-
-        return bestAdIndex;
+    uint256 bestAdIndex = 0;
+    uint256 bestScore = 0;
+    for (uint256 i = 0; i < scores.length; i++) {
+      if (scores[i] > bestScore) {
+        bestScore = scores[i];
+        bestAdIndex = i;
+      }
     }
 
-    function findBestAdPermitSealed(inEbool[] calldata encryptedUserVector, Permission memory permission) 
-        public view onlySender(permission) returns (uint256) {
-        euint8 [] memory encryptedScores = findBestAd(encryptedUserVector);
+    return bestAdIndex;
+  }
 
-        uint8[] memory scores = new uint8[](encryptedScores.length);
-        for (uint256 i = 0; i < encryptedScores.length; i++) {
-            scores[i] = FHE.decrypt(encryptedScores[i]);
-        }
+  function findBestAdPermitSealed(
+    inEbool[] calldata encryptedUserVector,
+    Permission memory permission
+  ) public view onlySender(permission) returns (uint256) {
+    euint8[] memory encryptedScores = findBestAd(encryptedUserVector);
 
-        uint256 bestAdIndex = 0;
-        uint256 bestScore = 0;
-        for (uint256 i = 0; i < scores.length; i++) {
-            if (scores[i] > bestScore) {
-                bestScore = scores[i];
-                bestAdIndex = i;
-            }
-        }
-
-        return bestAdIndex;
+    uint8[] memory scores = new uint8[](encryptedScores.length);
+    for (uint256 i = 0; i < encryptedScores.length; i++) {
+      scores[i] = FHE.decrypt(encryptedScores[i]);
     }
 
-    function getUserVector(address user) public view returns (bool[] memory) {
-        ebool[] memory encryptedUserVector = userVectors[user];
-        bool[] memory userVector = new bool[](encryptedUserVector.length);
-        for (uint256 i = 0; i < encryptedUserVector.length; i++) {
-            userVector[i] = FHE.decrypt(encryptedUserVector[i]);
-        }
-        return userVector;
+    uint256 bestAdIndex = 0;
+    uint256 bestScore = 0;
+    for (uint256 i = 0; i < scores.length; i++) {
+      if (scores[i] > bestScore) {
+        bestScore = scores[i];
+        bestAdIndex = i;
+      }
     }
+
+    return bestAdIndex;
+  }
+
+  function getUserVector(address user) public view returns (bool[] memory) {
+    ebool[] memory encryptedUserVector = userVectors[user];
+    bool[] memory userVector = new bool[](encryptedUserVector.length);
+    for (uint256 i = 0; i < encryptedUserVector.length; i++) {
+      userVector[i] = FHE.decrypt(encryptedUserVector[i]);
+    }
+    return userVector;
+  }
 }
